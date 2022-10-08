7a0c3899fd167327eb3e1f1c7c9434ba56861729
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-09 00:28:13 +0530

    input output processing added

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-var billamount=document.querySelector("#bill-amount")
-var cashamount=document.querySelector("#cash-given")
-var checkbbutton=document.querySelector("#buto")
+var billamount=document.querySelector("#bill-amount");
+var cashamount=document.querySelector("#cash-given");
+var checkbbutton=document.querySelector("#buto");
+var texto=document.querySelector("#error");
+const note=[2000,500,100,20,10,5,1];
+var notesnum=document.querySelectorAll(".no-of-notes")
 
+checkbbutton.addEventListener("click",function validatebillamount(){
+    texto.style.display="none";
+if(billamount.value>0)
+{
+    if(cashamount.value>billamount.value)
+    {
 
+        const amountreturned =cashamount.value-billamount.value;
+      calculate(amountreturned)
+    }
+    else{
+        texto.style.display="block";
+        texto.innerText="Bhai bartan dhulega kya";
+    }
+    }
+else
+{
+    texto.style.display="block";
+texto.innerText="Invalid bill amount";
 
-checkbbutton.addEventListener("click",)
+}
+})
+function calculate(ar){
+for(let i=0;i<note.length;i++){
+var noteesofeach=Math.trunc(ar/note[i]);
+    ar=ar%note[i];
+notesnum[i].innerText=noteesofeach;
+}
+}
