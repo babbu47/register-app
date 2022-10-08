786733afa61780f048bf5b2951d670b716b155e3
babbu47 <111335500+babbu47@users.noreply.github.com>
2022-10-08 23:31:15 +0530

    docs:text added

---
+++ app.js
@@ -0,0 +1,7 @@
+var billamount=document.querySelector("#bill-amount")
+var cashamount=document.querySelector("#cash-given")
+var checkbbutton=document.querySelector("#buto")
+
+
+
+checkbbutton.addEventListener("click",)
